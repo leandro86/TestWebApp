c36d828b0d8287a6fd81de52746a80fbac5bf9f1
Bernhard Richter <bernhard.richter@gmail.com>
2018-08-08 10:24:29 +0200

    Minor stuff

--- TestWebApp/Program.cs
+++ TestWebApp/Program.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using LightInject;
+﻿using LightInject;
 using LightInject.Microsoft.DependencyInjection;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace TestWebApp
 {
@@ -23,8 +16,7 @@ namespace TestWebApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseLightInject()
-                
+                .UseLightInject()                
                 .UseStartup<Startup>();
     }
 
@@ -37,26 +29,7 @@ namespace TestWebApp
     {
 
     }
-
-    //public class LightInjectServiceProviderFactory : IServiceProviderFactory<IServiceContainer>
-    //{
-    //    private IServiceCollection _services;
-    //    private ServiceContainer _serviceContainer;
-
-    //    public IServiceContainer CreateBuilder(IServiceCollection services)
-    //    {
-    //        _services = services;
-    //        _serviceContainer = new ServiceContainer(new ContainerOptions { EnablePropertyInjection = false});
-    //        return _serviceContainer;
-    //    }
-
-    //    public IServiceProvider CreateServiceProvider(IServiceContainer containerBuilder)
-    //    {
-    //        var provider = containerBuilder.CreateServiceProvider(_services);                      
-    //        return provider;
-    //    }
-    //}
-
+   
     public static class WebHostBuilderExtensions
     {
         public static IWebHostBuilder UseLightInject(this IWebHostBuilder hostBuilder)
